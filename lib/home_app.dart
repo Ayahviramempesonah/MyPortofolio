1d091e72f69e5e4e2a8e775f70f0fd169e6cd604
bayusatriapurnomo <bayusatriapurnomo@gmail.com>
2024-06-06 18:41:33 +0000

    add gridview

--- lib/home_app.dart
+++ lib/home_app.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 
-class HomeApp extends StatelessWidget {
+class HomeApp extends StatefulWidget {
   const HomeApp({super.key});
 
   @override
+  State<HomeApp> createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:3105817642.
+      appBar: AppBar(
+        title: Text('Home'),
+        leading:Icon(
+          Icons.menu,
+          color: Colors.black,
+        ) ,
+      ),
+     body: GridView.count(
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1643814353.
+        crossAxisCount: 2,
+        children: List.generate(10, (index) {
+          return Card(
+            child: Center(
+              child: Text('Item $index'),
+            ),
+          );
+        }),
+      ),
     );
   }
 }
